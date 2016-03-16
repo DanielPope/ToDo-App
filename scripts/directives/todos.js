95e06a7b2ecf8a90e48e8edc39b4566306ba4fb8
DanielPope <Dannie@Dannies-MacBook.local>
2016-03-16 05:47:39 -0400

    First Commit

---
+++ scripts/directives/todos.js
@@ -0,0 +1,11 @@
+angular.module('todoListApp')
+.directive('todos', function(){
+   return {
+    templateUrl:'templates/todos.html',
+     controller:'mainCtrl',
+     replace: true
+   }         
+ })
+
+
+
